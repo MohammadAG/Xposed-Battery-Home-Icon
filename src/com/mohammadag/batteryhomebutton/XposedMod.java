72405c3e2fd16e9145e3ad56833e475d5a9d8271
dieehard <dieehard@users.noreply.github.com>
2014-10-29 05:44:21 +0800

    Update XposedMod.java
    Add support for G3

--- src/com/mohammadag/batteryhomebutton/XposedMod.java
+++ src/com/mohammadag/batteryhomebutton/XposedMod.java
@@ -29,14 +29,20 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackage
 
 	private XSharedPreferences mPrefs;
 
+	
+	
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("com.android.systemui"))
 			return;
 
 		mPrefs = new XSharedPreferences("com.mohammadag.batteryhomebutton");
-
-		Class<?> NavigationBarView = XposedHelpers.findClass("com.android.systemui.statusbar.phone.NavigationBarView", lpparam.classLoader);
+		boolean isLGDevice = Build.MANUFACTURER.toLowerCase(Locale.getDefault()).equals("lge");
+		String NavigationBarViewClassName="com.android.systemui.statusbar.phone.NavigationBarView";
+		if (isLGDevice) {
+			NavigationBarViewClassName = "com.android.systemui.statusbar.phone.LGNavigationBarView";
+		}
+		Class<?> NavigationBarView = XposedHelpers.findClass(NavigationBarViewClassName, lpparam.classLoader);
 
 		XC_MethodHook hook = new XC_MethodHook() {
 			@Override
@@ -79,17 +85,19 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackage
 
 		XposedHelpers.findAndHookMethod(NavigationBarView, "onFinishInflate", hook);
 		XposedHelpers.findAndHookMethod(NavigationBarView, "reorient", hook);
-		XposedHelpers.findAndHookMethod(NavigationBarView, "getIcons", Resources.class, new XC_MethodHook() {
-			@Override
-			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				try {
-					XposedHelpers.setObjectField(param.thisObject, "mHomeIcon", mBatteryDrawable);
-					XposedHelpers.setObjectField(param.thisObject, "mHomeLandIcon", mBatteryDrawable);
-				} catch (NoSuchFieldError e) {
-
+		if (!isLGDevice) {
+			XposedHelpers.findAndHookMethod(NavigationBarView, "getIcons", Resources.class, new XC_MethodHook() {
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					try {
+						XposedHelpers.setObjectField(param.thisObject, "mHomeIcon", mBatteryDrawable);
+						XposedHelpers.setObjectField(param.thisObject, "mHomeLandIcon", mBatteryDrawable);
+					} catch (NoSuchFieldError e) {
+	
+					}
 				}
-			}
-		});
+			});
+		}
 		XposedBridge.hookAllConstructors(NavigationBarView, new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -144,7 +152,7 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackage
 			}
 		});
 
-		if (Build.MANUFACTURER.toLowerCase(Locale.getDefault()).equals("lge")) {
+		if (isLGDevice) {
 			try {
 				Class<?> LGHomeButton = XposedHelpers.findClass("com.lge.navigationbar.HomeButton", lpparam.classLoader);
 				XposedHelpers.findAndHookConstructor(LGHomeButton, Context.class, AttributeSet.class, int.class, boolean.class, new XC_MethodHook() {
@@ -154,6 +162,26 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackage
 						createBatteryIfNeeded(view);
 
 						view.setImageDrawable(mBatteryDrawable);
+					}
+				});
+			} catch (Throwable t) {
+				XposedBridge.log("BatteryHomeIcon: Failed to apply LG hook: " + t.getMessage());
+				t.printStackTrace();
+			}
+			
+			try {
+				Class<?> NavigationThemeResource = XposedHelpers.findClass("com.lge.navigationbar.NavigationThemeResource", lpparam.classLoader);
+				XposedHelpers.findAndHookMethod(NavigationThemeResource, "getThemeResource", View.class, new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						try {
+							ImageView v = (ImageView)param.args[0];
+							if (v.getClass().getSimpleName().equalsIgnoreCase("HomeButton")) {
+								createBatteryIfNeeded(v);
+								param.setResult(mBatteryDrawable);
+							}							
+						} catch (NoSuchFieldError e) {
+						}
 					}
 				});
 			} catch (Throwable t) {
